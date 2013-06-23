a696517dfaa7c298541f896b0be393a0fea64b20
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-06-23 14:19:20 +0200

    HW1: get the 100 first prime numbers

--- HW1/part2.js
+++ HW1/part2.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 var fs = require('fs'),
     primes = [ 2 ],
-    i, number,
+    i, number = 3,
     isPrime,
     outfile = "part2.txt",
     out = "";
 
-for (number = 3; number <= 100; number++) {
+while (primes.length <= 100) {
   isPrime = true;
   for (i in primes) {
     if (number % primes[i] == 0) {
@@ -17,6 +17,8 @@ for (number = 3; number <= 100; number++) {
   if (isPrime) {
     primes.push(number);
   }
+  // 2 is the only even prime nmber
+  number += 2;
 }
 
 for (i in primes) {
